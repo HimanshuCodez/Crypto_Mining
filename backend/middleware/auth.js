45d24f17de2c7c749b9c673b301dd6cb5f20e93d
Himanshu gaur <himanshugaur055@gmail.com>
2025-09-06 18:56:19 +0530

    auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -17,6 +17,15 @@ const auth = async (req, res, next) => {
     }
 };
 
+export const isAdmin = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(403).json({ message: 'Not authorized as an admin' });
+    }
+};
+
 export default auth;
+
 
 
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,20 +1,21 @@
-import auth from '../middleware/auth.js';
+import auth, { isAdmin } from '../middleware/auth.js';
 import { getBarcode,  upload,updateBarcode, submitPayment, approvePayment, rejectPayment, getPendingPayments, getAllUsers, approveMiningInvestment, getPendingMiningInvestments, getMiningInvestments, payAllInvestmentsProfit, getPaymentStatus } from '../controllers/admin.js';
 import express from 'express';
 
 const router = express.Router();
 
-router.get('/users', auth, getAllUsers);
-router.get('/barcode', getBarcode);
-router.post('/barcode', auth, updateBarcode);
+router.get('/users', auth, isAdmin, getAllUsers);
+router.get('/barcode', auth, isAdmin, getBarcode);
+router.post('/barcode', auth, isAdmin, updateBarcode);
 router.post('/payments/submit', auth, upload.single('screenshot'), submitPayment);
-router.get('/payments/pending', auth,  getPendingPayments);
-router.get('/payments/status/:userId', auth, getPaymentStatus);
-router.get('/mining-investments/pending', auth, getPendingMiningInvestments);
-router.get('/mining-investments', auth, getMiningInvestments);
-router.post('/payments/approve/:paymentId', auth,approvePayment);
-router.post('/payments/reject/:paymentId', auth,  rejectPayment);
-router.post('/mining-investments/approve/:transactionId', auth, approveMiningInvestment);
-router.post('/mining-investments/pay-all', auth, payAllInvestmentsProfit);
+router.get('/payments/pending', auth, isAdmin,  getPendingPayments);
+router.get('/payments/status/:userId', auth, isAdmin, getPaymentStatus);
+router.get('/mining-investments/pending', auth, isAdmin, getPendingMiningInvestments);
+router.get('/mining-investments', auth, isAdmin, getMiningInvestments);
+router.post('/payments/approve/:paymentId', auth, isAdmin,approvePayment);
+router.post('/payments/reject/:paymentId', auth, isAdmin,  rejectPayment);
+router.post('/mining-investments/approve/:transactionId', auth, isAdmin, approveMiningInvestment);
+router.post('/mining-investments/pay-all', auth, isAdmin, payAllInvestmentsProfit);
 
 export default router;
+
