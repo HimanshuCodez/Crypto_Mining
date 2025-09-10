321696468a132169a8824a65d3ddacf863290857
Himanshu gaur <himanshugaur055@gmail.com>
2025-09-10 16:07:25 +0530

    to wallet income

--- client/src/Pages/Components/Transfer/ToIncomeWallet.jsx
+++ client/src/Pages/Components/Transfer/ToIncomeWallet.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useAuthStore from '../../../store/authStore';
 import api from '../../../api/axios';
 import { toast } from 'react-toastify';
